fa6fa5d4b2853fef5cc8cad900ea94660ac09fd2
Jake Soenneker <jake@soenneker.com>
2025-01-14 12:47:43 -0600

    Deps

--- src/Registrars/Sha3UtilRegistrar.cs
+++ src/Registrars/Sha3UtilRegistrar.cs
@@ -12,16 +12,20 @@ public static class Sha3UtilRegistrar
     /// <summary>
     /// Adds <see cref="ISha3Util"/> as a singleton service. <para/>
     /// </summary>
-    public static void AddSha3UtilAsSingleton(this IServiceCollection services)
+    public static IServiceCollection AddSha3UtilAsSingleton(this IServiceCollection services)
     {
         services.TryAddSingleton<ISha3Util, Sha3Util>();
+
+        return services;
     }
 
     /// <summary>
     /// Adds <see cref="ISha3Util"/> as a scoped service. <para/>
     /// </summary>
-    public static void AddSha3UtilAsScoped(this IServiceCollection services)
+    public static IServiceCollection AddSha3UtilAsScoped(this IServiceCollection services)
     {
         services.TryAddScoped<ISha3Util, Sha3Util>();
+
+        return services;
     }
 }
